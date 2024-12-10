6cff5d0a76fd42cb068d60d49a9c8b337723120f
Devaesh <devaesh@Devaeshs-MacBook-Pro.local>
2024-12-10 13:47:47 +0530

    Ticket reducer created and functionailty implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./styles.css";
+import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
 
 function App() {
   const initalState = { tickets: [] };
+
+  const [state, dispatch] = useReducer(ticketReducer, initalState);
 
   return (
     <div className="App">
       <div className="container">
         <h1>Bug Blaster</h1>
-        <TicketForm></TicketForm>
+        <TicketForm dispatch={dispatch}></TicketForm>
       </div>
     </div>
   );
