9e18b92301bbbd6dafbae67e7408746ceb63ff87
Devaesh <devaesh@Devaeshs-MacBook-Pro.local>
2024-12-16 19:44:03 +0530

    Deleting an under-edit ticket and cancelling an edit procedure functionalities added

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -44,6 +44,13 @@ export default function TicketForm({ dispatch, editingTicket }) {
     clearForm();
   };
 
+  const handleCancelEdit = () => {
+    dispatch({
+      type: "CLEAR_EDITING_TICKET",
+    });
+
+    clearForm();
+  };
   return (
     <form onSubmit={handleSubmit} className="ticket-form">
       <div>
@@ -83,6 +90,12 @@ export default function TicketForm({ dispatch, editingTicket }) {
       <button type="submit" className="button">
         Submit
       </button>
+
+      {editingTicket && (
+        <button className="button" onClick={handleCancelEdit}>
+          Cancel edit
+        </button>
+      )}
     </form>
   );
 }
